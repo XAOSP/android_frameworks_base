fd414f49063143caf4f2c9098e424024435fa556
Martin J. Pollard <martin0566@gmail.com>
2022-11-23 12:16:06 -0500

    base: Enable double tap to wake by default

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -251,7 +251,7 @@ public final class PowerManagerService extends SystemService
     static final long MIN_LONG_WAKE_CHECK_INTERVAL = 60*1000;
 
     // Default setting for double tap to wake.
-    private static final int DEFAULT_DOUBLE_TAP_TO_WAKE = 0;
+    private static final int DEFAULT_DOUBLE_TAP_TO_WAKE = 1;
 
     // System property indicating that the screen should remain off until an explicit user action
     private static final String SYSTEM_PROPERTY_QUIESCENT = "ro.boot.quiescent";
