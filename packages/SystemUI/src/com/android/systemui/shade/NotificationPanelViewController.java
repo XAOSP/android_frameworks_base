62b7e3a59ef6a6a25e134d84ba22ae0b79b6bc50
Bin Huang <ot_bin.huang@mediatek.com>
2023-10-13 17:26:13 +0800

    Avoid keyguard fling transition interrupt for timing issue
    For some low-end or bad performance devices, sometimes keyguard
    flingToHeight animation still not complete, at this moment if received
    the onStartedWakingUp while try turn screen on, it will cause the
    remote SystemUI keyguard dismiss transition can't run.
    
    Before execute a fling animation of Keyguard, set the animation execution time to 0 determines that it is Screen Off state.
    This solution makes Screen On can not affect animation.
    
    Bug: 301196699
    Test: run cts KeyguardTests
    
    Google: 2787913
    Change-Id: Ic7be1b5e41814adb44ce79857b79283040b4e3a4
    Merged-In: I4743c0ad53f89ee2fdc2f8e9eed864fe79f3bfc3

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -639,6 +639,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     private int mDreamingToLockscreenTransitionTranslationY;
     private int mLockscreenToDreamingTransitionTranslationY;
     private int mGoneToDreamingTransitionTranslationY;
+    private boolean mForceFlingAnimationForTest = false;
     private SplitShadeStateController mSplitShadeStateController;
 
     private final PowerManagerInternal mLocalPowerManager;
@@ -2226,8 +2227,16 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                 }
             }
         });
+        if (!mScrimController.isScreenOn() && !mForceFlingAnimationForTest) {
+            animator.setDuration(1);
+        }
         setAnimator(animator);
         animator.start();
+    }
+
+    @VisibleForTesting
+    void setForceFlingAnimationForTest(boolean force) {
+        mForceFlingAnimationForTest = force;
     }
 
     @VisibleForTesting
